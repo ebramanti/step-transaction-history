56231d206ce7e1406e4034623f0997de0d1a5f1e
Edward Bramanti <edward@bramanti.org>
2021-05-06 15:33:05 -0700

    Support pagination in TransactionHistory component - Extract swap logic into async function - Add functions for going to next/previous pages in footer

--- src/components/TransactionHistory/TransactionHistory.tsx
+++ src/components/TransactionHistory/TransactionHistory.tsx
@@ -1,5 +1,5 @@
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
-import { PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey, TransactionSignature } from "@solana/web3.js";
 import { Button, Table } from "antd";
 import { ColumnsType } from "antd/lib/table";
 import React, { FC, useEffect, useState } from "react";
@@ -23,6 +23,70 @@ import { PoolIcon } from "../TokenIcon";
 
 const TRANSACTION_LIMIT = 20;
 
+const getSwaps = async (
+  connection: Connection,
+  publicKey: PublicKey,
+  before?: TransactionSignature
+): Promise<Swap[]> => {
+  const filteredTransactions = await getFilteredTransactions(
+    connection,
+    publicKey,
+    {
+      programIds: PROGRAM_ID_SET,
+      limit: TRANSACTION_LIMIT,
+      before: before,
+    }
+  );
+
+  const swaps = filteredTransactions.map<Swap>((transaction) => {
+    let data: Swap | undefined;
+    // TODO: Rework getFilteredTransactions to capture transaction data source
+    for (let instruction of transaction.transaction.message.instructions) {
+      const rawProgramId = instruction.programId.toBase58();
+      if (rawProgramId === SERUM_SWAP_PROGRAM_ID) {
+        data = getSerumData(transaction);
+        break;
+      }
+
+      if (rawProgramId === ORCA_SWAP_PROGRAM_ID) {
+        data = getOrcaData(transaction);
+        break;
+      }
+    }
+
+    if (!data) {
+      throw new Error("Unable to parse transaction data");
+    }
+
+    return {
+      signature: data.signature,
+      platform: data.platform,
+      type: data.type,
+      fromAmount: data.fromAmount,
+      fromSource: data.fromSource,
+      toSource: data.toSource,
+      toAmount: data.toAmount,
+    };
+  });
+
+  const sourceSet = new Set(
+    swaps.flatMap((swap) => [swap.fromSource, swap.toSource])
+  );
+
+  const accounts = await getMultipleAccounts(
+    connection,
+    Array.from(sourceSet),
+    "confirmed"
+  );
+  accounts.keys.forEach((key, index) => {
+    const account = accounts.array[index];
+    if (account) {
+      cache.add(new PublicKey(key), account, TokenAccountParser);
+    }
+  });
+  return swaps;
+};
+
 export const TransactionHistory: FC = () => {
   const { wallet } = useWallet();
   const publicKey = wallet?.publicKey;
@@ -30,79 +94,51 @@ export const TransactionHistory: FC = () => {
   const { tokenMap } = useConnectionConfig();
 
   const [swaps, setSwaps] = useState<Swap[]>([]);
+  const [offset, setOffset] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
-    (async () => {
-      if (publicKey) {
-        const filteredTransactions = await getFilteredTransactions(
-          connection,
-          publicKey,
-          {
-            programIds: PROGRAM_ID_SET,
-            limit: TRANSACTION_LIMIT,
-          }
-        );
-
-        const swaps = filteredTransactions.map<Swap>((transaction) => {
-          let data: Swap | undefined;
-          // TODO: Rework getFilteredTransactions to capture transaction data source
-          for (let instruction of transaction.transaction.message
-            .instructions) {
-            const rawProgramId = instruction.programId.toBase58();
-            if (rawProgramId === SERUM_SWAP_PROGRAM_ID) {
-              data = getSerumData(transaction);
-              break;
-            }
-
-            if (rawProgramId === ORCA_SWAP_PROGRAM_ID) {
-              data = getOrcaData(transaction);
-              break;
-            }
-          }
-
-          if (!data) {
-            throw new Error("Unable to parse transaction data");
-          }
-
-          return {
-            signature: data.signature,
-            platform: data.platform,
-            type: data.type,
-            fromAmount: data.fromAmount,
-            fromSource: data.fromSource,
-            toSource: data.toSource,
-            toAmount: data.toAmount,
-          };
-        });
-
-        const sourceSet = new Set(
-          swaps.flatMap((swap) => [swap.fromSource, swap.toSource])
-        );
-
-        const accounts = await getMultipleAccounts(
-          connection,
-          Array.from(sourceSet),
-          "confirmed"
-        );
-        accounts.keys.forEach((key, index) => {
-          const account = accounts.array[index];
-          if (account) {
-            cache.add(new PublicKey(key), account, TokenAccountParser);
-          }
-        });
+    if (publicKey) {
+      setLoading(true);
+      getSwaps(connection, publicKey).then((swaps) => {
         setSwaps(swaps);
-        console.log(swaps);
-
-        // TODO: Remove, using this to manually test before argument
-        // @ts-ignore
-        window.getFilteredTransactions = (before: string) =>
-          getFilteredTransactions(connection, publicKey, {
-            before: before,
-            programIds: PROGRAM_ID_SET,
-          });
-      }
-    })();
+        setOffset(0);
+        setLoading(false);
+      });
+    }
   }, [publicKey, connection]);
+
+  const onPagePrevious = () => {
+    setOffset((previousOffset) => previousOffset - TRANSACTION_LIMIT);
+  };
+
+  const onPageNext = () => {
+    const updatedOffset = offset + TRANSACTION_LIMIT;
+    const lastElementInPreviousOffset = updatedOffset - 1;
+
+    if (
+      swaps.slice(updatedOffset, updatedOffset + TRANSACTION_LIMIT).length ===
+      TRANSACTION_LIMIT
+    ) {
+      return setOffset(updatedOffset);
+    }
+
+    if (publicKey) {
+      setLoading(true);
+      getSwaps(
+        connection,
+        publicKey,
+        swaps[lastElementInPreviousOffset].signature
+      ).then((swaps) => {
+        setSwaps((currentSwaps) => {
+          setOffset(updatedOffset);
+          const updatedSwaps = currentSwaps.concat(swaps);
+          return updatedSwaps;
+        });
+        setLoading(false);
+      });
+    }
+  };
 
   const columns: ColumnsType<object> = [
     {
@@ -171,15 +207,20 @@ export const TransactionHistory: FC = () => {
 
   return (
     <Table
-      dataSource={swaps}
+      dataSource={swaps.slice(offset, offset + TRANSACTION_LIMIT)}
       columns={columns}
+      pagination={false}
+      loading={loading}
       footer={() => {
         if (swaps.length > 0) {
           return (
-            // TODO: Implement functionality for these buttons
             <div style={{ textAlign: "right" }}>
-              <Button icon={<LeftOutlined />} />{" "}
-              <Button icon={<RightOutlined />} />
+              <Button
+                icon={<LeftOutlined />}
+                disabled={offset === 0}
+                onClick={onPagePrevious}
+              />{" "}
+              <Button icon={<RightOutlined />} onClick={onPageNext} />
             </div>
           );
         }
