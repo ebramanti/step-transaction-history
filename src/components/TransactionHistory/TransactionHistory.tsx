2d88078c678c35c6ad8e3fbb7972aa26faeec6e8
Edward Bramanti <edward@bramanti.org>
2021-05-06 00:23:20 -0700

    Add TransactionHistory component - Migrate loader effect to TransactionHistory - Properly load all source addresses into cache - Custom rendering for asset/amount fields

---
+++ src/components/TransactionHistory/TransactionHistory.tsx
@@ -0,0 +1,185 @@
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
+import { PublicKey } from "@solana/web3.js";
+import { Button, Table } from "antd";
+import { ColumnsType } from "antd/lib/table";
+import React, { FC, useEffect, useState } from "react";
+import {
+  cache,
+  getMultipleAccounts,
+  TokenAccountParser,
+} from "../../contexts/accounts";
+import { useConnection, useConnectionConfig } from "../../contexts/connection";
+import { useWallet } from "../../contexts/wallet";
+import {
+  getFilteredTransactions,
+  getOrcaData,
+  getSerumData,
+  ORCA_SWAP_PROGRAM_ID,
+  PROGRAM_ID_SET,
+  SERUM_SWAP_PROGRAM_ID,
+  Swap,
+} from "../../transactionHistory";
+import { PoolIcon } from "../TokenIcon";
+
+const TRANSACTION_LIMIT = 20;
+
+export const TransactionHistory: FC = () => {
+  const { wallet } = useWallet();
+  const publicKey = wallet?.publicKey;
+  const connection = useConnection();
+  const { tokenMap } = useConnectionConfig();
+
+  const [swaps, setSwaps] = useState<Swap[]>([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      getFilteredTransactions(connection, publicKey, {
+        programIds: PROGRAM_ID_SET,
+        limit: TRANSACTION_LIMIT,
+      }).then((filteredTransactions) => {
+        const swaps = filteredTransactions.map<Swap>((transaction) => {
+          let data: Swap | undefined;
+          // TODO: Rework getFilteredTransactions to capture transaction data source
+          for (let instruction of transaction.transaction.message
+            .instructions) {
+            const rawProgramId = instruction.programId.toBase58();
+            if (rawProgramId === SERUM_SWAP_PROGRAM_ID) {
+              data = getSerumData(transaction);
+              break;
+            }
+
+            if (rawProgramId === ORCA_SWAP_PROGRAM_ID) {
+              data = getOrcaData(transaction);
+              break;
+            }
+          }
+
+          if (!data) {
+            throw new Error("Unable to parse transaction data");
+          }
+
+          return {
+            signature: data.signature,
+            platform: data.platform,
+            type: data.type,
+            fromAmount: data.fromAmount,
+            fromSource: data.fromSource,
+            toSource: data.toSource,
+            toAmount: data.toAmount,
+          };
+        });
+
+        const sourceSet = new Set(
+          swaps.flatMap((swap) => [swap.fromSource, swap.toSource])
+        );
+
+        // Example implementation of how to load token data from source addresses in swap
+        getMultipleAccounts(
+          connection,
+          Array.from(sourceSet),
+          "confirmed"
+        ).then((accounts) => {
+          accounts.keys.forEach((key, index) => {
+            const account = accounts.array[index];
+            if (account) {
+              cache.add(new PublicKey(key), account, TokenAccountParser);
+            }
+          });
+          setSwaps(swaps);
+          console.log(swaps);
+        });
+      });
+
+      // TODO: Remove, using this to manually test before argument
+      // @ts-ignore
+      window.getFilteredTransactions = (before: string) =>
+        getFilteredTransactions(connection, publicKey, {
+          before: before,
+          programIds: PROGRAM_ID_SET,
+        });
+    }
+  }, [publicKey, connection]);
+
+  const columns: ColumnsType<object> = [
+    {
+      title: "Platform",
+      dataIndex: "platform",
+      key: "platform",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type",
+    },
+    {
+      title: "Asset",
+      key: "asset",
+      render: (value: Swap) => {
+        const { toSource, fromSource } = value;
+        const fromSourceMintAddress: PublicKey | undefined = cache.get(
+          fromSource
+        )?.info?.mint;
+        const toSourceMintAddress: PublicKey | undefined = cache.get(toSource)
+          ?.info?.mint;
+        if (fromSourceMintAddress && toSourceMintAddress) {
+          const fromToken = tokenMap.get(fromSourceMintAddress.toBase58());
+          const toToken = tokenMap.get(toSourceMintAddress.toBase58());
+          return (
+            <div style={{ display: "inline-flex", alignItems: "center" }}>
+              <PoolIcon
+                mintA={fromSourceMintAddress.toBase58()}
+                mintB={toSourceMintAddress.toBase58()}
+              />
+              {fromToken?.symbol} - {toToken?.symbol}
+            </div>
+          );
+        } else {
+          return null;
+        }
+      },
+    },
+    {
+      title: "Amount",
+      key: "amount",
+      render: (value: Swap) => {
+        const { toSource, fromSource, toAmount, fromAmount } = value;
+        const fromSourceMintAddress: PublicKey | undefined = cache.get(
+          fromSource
+        )?.info?.mint;
+        const toSourceMintAddress: PublicKey | undefined = cache.get(toSource)
+          ?.info?.mint;
+        if (fromSourceMintAddress && toSourceMintAddress) {
+          const fromToken = tokenMap.get(fromSourceMintAddress.toBase58());
+          const toToken = tokenMap.get(toSourceMintAddress.toBase58());
+          if (fromToken && toToken) {
+            const fromTokenAmount =
+              parseFloat(fromAmount) * 10 ** -fromToken.decimals;
+            const toTokenAmount =
+              parseFloat(toAmount) * 10 ** -toToken.decimals;
+            return `-${fromTokenAmount} / +${toTokenAmount}`;
+          }
+        } else {
+          return null;
+        }
+      },
+    },
+  ];
+
+  return (
+    <Table
+      dataSource={swaps}
+      columns={columns}
+      footer={() => {
+        if (swaps.length > 0) {
+          return (
+            // TODO: Implement functionality for these buttons
+            <div style={{ textAlign: "right" }}>
+              <Button icon={<LeftOutlined />} />{" "}
+              <Button icon={<RightOutlined />} />
+            </div>
+          );
+        }
+      }}
+    />
+  );
+};
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,33 +1,16 @@
-import { ParsedConfirmedTransaction, PublicKey } from "@solana/web3.js";
 import { Button, Col, Row } from "antd";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { ConnectButton } from "../../components/ConnectButton";
 import { TokenIcon } from "../../components/TokenIcon";
-import {
-  cache,
-  getMultipleAccounts,
-  TokenAccountParser,
-} from "../../contexts/accounts";
-import { useConnection, useConnectionConfig } from "../../contexts/connection";
+import { TransactionHistory } from "../../components/TransactionHistory/TransactionHistory";
+import { useConnectionConfig } from "../../contexts/connection";
 import { useMarkets } from "../../contexts/market";
-import { useWallet } from "../../contexts/wallet";
 import { useUserBalance, useUserTotalBalance } from "../../hooks";
-import {
-  getFilteredTransactions,
-  getOrcaData,
-  getSerumData,
-  ORCA_SWAP_PROGRAM_ID,
-  PROGRAM_ID_SET,
-  SERUM_SWAP_PROGRAM_ID,
-} from "../../transactionHistory";
 import { WRAPPED_SOL_MINT } from "../../utils/ids";
 import { formatUSD } from "../../utils/utils";
 
 export const HomeView = () => {
-  const { wallet } = useWallet();
-  const publicKey = wallet?.publicKey;
-  const connection = useConnection();
   const { marketEmitter, midPriceInUSD } = useMarkets();
   const { tokenMap } = useConnectionConfig();
   const SRM_ADDRESS = "SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt";
@@ -36,9 +19,6 @@ export const HomeView = () => {
   const STEP = useUserBalance(STEP_ADDRESS);
   const SOL = useUserBalance(WRAPPED_SOL_MINT);
   const { balanceInUSD: totalBalanceInUSD } = useUserTotalBalance();
-  const [transactions, setTransactions] = useState<
-    ParsedConfirmedTransaction[]
-  >([]);
 
   useEffect(() => {
     const refreshTotal = () => {};
@@ -53,59 +33,6 @@ export const HomeView = () => {
       dispose();
     };
   }, [marketEmitter, midPriceInUSD, tokenMap]);
-
-  useEffect(() => {
-    if (publicKey) {
-      getFilteredTransactions(connection, publicKey, {
-        programIds: PROGRAM_ID_SET,
-      }).then((filteredTransactions) => {
-        setTransactions(filteredTransactions);
-        console.log(filteredTransactions);
-        const serumTransaction = filteredTransactions.find((data) => {
-          return data.transaction.message.instructions.some(
-            (i) => i.programId.toBase58() === SERUM_SWAP_PROGRAM_ID
-          );
-        });
-        const orcaTransaction = filteredTransactions.find((data) => {
-          return data.transaction.message.instructions.some(
-            (i) => i.programId.toBase58() === ORCA_SWAP_PROGRAM_ID
-          );
-        });
-        console.log(serumTransaction);
-        const serumData = getSerumData(serumTransaction!);
-        console.log(serumData);
-        const orcaData = getOrcaData(orcaTransaction!);
-        console.log(orcaData);
-
-        // Example implementation of how to load token data from source addresses in swap
-        getMultipleAccounts(
-          connection,
-          [
-            serumData.fromSource,
-            serumData.toSource,
-            orcaData.fromSource,
-            orcaData.toSource,
-          ],
-          "confirmed"
-        ).then((accounts) => {
-          accounts.keys.forEach((key, index) => {
-            const account = accounts.array[index];
-            if (account) {
-              cache.add(new PublicKey(key), account, TokenAccountParser);
-            }
-          });
-        });
-      });
-
-      // TODO: Remove, using this to manually test before argument
-      // @ts-ignore
-      window.getFilteredTransactions = (before: string) =>
-        getFilteredTransactions(connection, publicKey, {
-          before: before,
-          programIds: PROGRAM_ID_SET,
-        });
-    }
-  }, [publicKey, connection]);
 
   return (
     <Row gutter={[16, 16]} align="middle">
@@ -123,6 +50,8 @@ export const HomeView = () => {
           <TokenIcon mintAddress={STEP_ADDRESS} /> STEP: {STEP.balance} (
           {formatUSD.format(SRM?.balanceInUSD)})
         </h2>
+        <br />
+        <TransactionHistory />
       </Col>
 
       <Col span={12}>
