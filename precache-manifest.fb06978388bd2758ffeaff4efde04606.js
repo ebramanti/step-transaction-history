52891b4bdbe2b23102395c7bbc955aa902ef8016
Edward Bramanti <edward@bramanti.org>
2021-05-06 17:06:55 -0700

    Updates

---
+++ precache-manifest.fb06978388bd2758ffeaff4efde04606.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3881028e8f1c9525968ffec4d72152dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c24251db6ae230f22888",
+    "url": "./static/css/main.d27ecd4c.chunk.css"
+  },
+  {
+    "revision": "7688b7d7e9144bc00a62",
+    "url": "./static/js/2.35979fa6.chunk.js"
+  },
+  {
+    "revision": "ca228484939fe4d1fc04f68ed86646c0",
+    "url": "./static/js/2.35979fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24251db6ae230f22888",
+    "url": "./static/js/main.b5d7c8fc.chunk.js"
+  },
+  {
+    "revision": "da503e7975902b161ea8",
+    "url": "./static/js/runtime-main.7d63d1b5.js"
+  },
+  {
+    "revision": "a4e80c1e977416ec45d808a3268892b7",
+    "url": "./static/media/builton.a4e80c1e.svg"
+  }
+]);
