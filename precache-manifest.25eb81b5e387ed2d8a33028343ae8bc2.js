c70f457465e1b5ec39e89ad5545684c3062aaa34
Edward Bramanti <edward@bramanti.org>
2021-05-06 21:43:18 -0700

    Updates

--- precache-manifest.25eb81b5e387ed2d8a33028343ae8bc2.js
+++ precache-manifest.25eb81b5e387ed2d8a33028343ae8bc2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ad0bb16d740f8ac798bde7577148c5f",
+    "revision": "d0538a8606c38ba11d9200b6e4ffad5f",
     "url": "./index.html"
   },
   {
-    "revision": "211a2b35ac6a28f1a9a1",
+    "revision": "b4ed986013c4cce1722f",
     "url": "./static/css/main.d27ecd4c.chunk.css"
   },
   {
-    "revision": "9763e8d83cd6c29f2011",
-    "url": "./static/js/2.8bf1269c.chunk.js"
+    "revision": "bc63e32b20e467694efd",
+    "url": "./static/js/2.0d6a0c6f.chunk.js"
   },
   {
     "revision": "ca228484939fe4d1fc04f68ed86646c0",
-    "url": "./static/js/2.8bf1269c.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.0d6a0c6f.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "211a2b35ac6a28f1a9a1",
-    "url": "./static/js/main.a33bcfd2.chunk.js"
+    "revision": "b4ed986013c4cce1722f",
+    "url": "./static/js/main.55bb11a2.chunk.js"
   },
   {
     "revision": "da503e7975902b161ea8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3076dc00befdd83d2640d6d2387974b2.js"
+  "./precache-manifest.25eb81b5e387ed2d8a33028343ae8bc2.js"
 );
 
 self.addEventListener('message', (event) => {
